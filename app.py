737ce7e5306dc8fc257d8728d37a26f5f0c10f80
Akhil Lawrence <akhilputhiry@gmail.com>
2019-03-24 20:53:10 +0530

    updated code to pull user info and access info

--- app.py
+++ app.py
@@ -1,59 +1,59 @@
+import json
+import base64
 import requests
+from urllib.parse import urlencode
+from flask import Flask, request, redirect
 
-from flask import Flask, g, request, Response
-from flask_oidc import OpenIDConnect
 
 # create flask app
 app = Flask(__name__)
-app.config.update({
-    'TESTING': True,
-    'DEBUG': True,
-    'SECRET_KEY': '8896958076629c1711b991c4552e8c6616b9e2a94a2fcb7c038fd10b049f30ce',
-    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
-    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
-    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
-    'OIDC_USER_INFO_ENABLED': True,
-    'OIDC_OPENID_REALM': 'flask-demo',
-    'OIDC_SCOPES': ['openid', 'email', 'profile'],
-    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
-    'OIDC_VALID_ISSUERS': ['http://keycloak.dev.lti-mosaic.com/auth/realms/demo-env']
-})
-
-
-# create openid client
-oidc = OpenIDConnect(app)
 
 
 @app.route('/login')
-@oidc.require_login
-def home():
+def login():
+    """ Endpoint to initiate login """
+    arguments = urlencode({
+        'client_id': 'flask-app',
+        'state': 'state123456789',
+        'response_type': 'code',
+        'scope': 'openid profile client_roles',
+        'redirect_uri': 'http://localhost:5000/callback',
+    })
+    auth_uri = 'https://keycloak.dev.lti-mosaic.com/auth/realms/akhil-poc/protocol/openid-connect/auth'
+    endpoint = auth_uri + '?' + arguments
+    return redirect(endpoint)
 
-    # fetch user information
-    user_info = oidc.user_getinfo(['email', 'sub', 'profile'])
-    return 'Email id of the logged in user is: {}'.format(user_info.get('email'))
+
+def fix_padding(data):
+    """ correct padding for base64 encoding """
+    required_padding = len(data) % 4
+    return data + ('=' * required_padding)
 
 
-@app.route('/profiling')
-@oidc.require_login
-def protected():
-
-    # fetch user information
-    user_info = oidc.user_getinfo(['email', 'sub', 'profile'])
-
-    # fetch access token
-    payload = {'client_id': 'flask-app', 'grant_type': 'client_credentials', 'client_secret': '53d6d03b-1111-40ab-b308-12b43ef8eaf0'}
-    access_token = requests.post('http://keycloak.dev.lti-mosaic.com/auth/realms/demo-env/protocol/openid-connect/token', data=payload).json().get('access_token')
-
-    # permission checking
-    payload = {"resources":[{"uri": request.path}], "userId": user_info.get('sub')}
-    headers = {"Authorization": "Bearer {}".format(access_token)}
-    result = requests.post('http://keycloak.dev.lti-mosaic.com/auth/admin/realms/demo-env/clients/bca0a1d5-7d5e-4d39-aa91-b95f432a6be8/authz/resource-server/policy/evaluate', json=payload, headers=headers).json().get('status')
-    if result != 'PERMIT':
-        return Response("You are doomed", status=401)
-
-    # process the request
-    return 'Howdy'
+@app.route('/callback')
+def callback():
+    """ Endpoint to retrieve logged in user info """
+    code = request.args.get('code')
+    data = {
+        'code': code,
+        'grant_type': 'authorization_code',
+        'client_id': 'flask-app',
+        'redirect_uri': 'http://localhost:5000/callback',
+        'client_secret': '1f49f057-bbf9-4389-a90f-3c5972f5564a'
+    }
+    token_uri = 'https://keycloak.dev.lti-mosaic.com/auth/realms/akhil-poc/protocol/openid-connect/token'
+    response = requests.post(token_uri, data=data)
+    response.raise_for_status()
+    user_tokens = response.json()
+    _, access_info, _ = user_tokens['access_token'].split('.')
+    _, user_info, _ = user_tokens['id_token'].split('.')
+    access_info = fix_padding(access_info)
+    user_info = fix_padding(user_info)
+    return json.dumps({
+        'access_info': json.loads(base64.b64decode(access_info)),
+        'user_info': json.loads(base64.b64decode(user_info)),
+    })
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
