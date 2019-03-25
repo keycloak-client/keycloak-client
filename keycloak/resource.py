b2b0a4a3f5acd9234eb1304ae2c2a063e4f1b6ed
Akhil Lawrence <akhilputhiry@gmail.com>
2019-03-25 15:18:07 +0530

    added crud interface

--- keycloak/resource.py
+++ keycloak/resource.py
@@ -32,9 +32,30 @@ class ResourceMixin(object):
 
         return response.json()
 
+    @property
+    def headers(self):
+        """ Common headers used within the class """
+        return {
+            'Authorization': 'Bearer {}'.format(self.pat['access_token'])
+        }
+
+    def list_resource(self):
+        """
+        Method to list all resources associated with the client
+
+        Returns:
+            dict
+        """
+
+        # create resource
+        response = requests.get(self.config['resource_endpoint'], headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()
+
     def create_resource(self, name, scopes=[]):
         """
-        Method to create resource in the keycloak server
+        Method to create resource
 
         Args:
             name (str): name of the resource
@@ -47,13 +68,66 @@ class ResourceMixin(object):
             'scopes': scopes,
         }
 
-        # prepare headers
-        headers = {
-            'Authorization': 'Bearer {}'.format(self.pat['access_token'])
-        }
+        # create resource
+        response = requests.post(self.config['resource_endpoint'], json=payload, headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def read_resource(self, resource_id):
+        """
+        Method to read resource
+
+        Args:
+            resource_id (str): id of the resource to be read
+        """
+
+        # prepare endpoint
+        endpoint = self.config['resource_endpoint'] + resource_id
 
         # create resource
-        response = requests.post(self.config['resource_endpoint'], json=payload, headers=headers)
+        response = requests.get(endpoint, headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def update_resource(self, resource_id, name, scopes=[]):
+        """
+        Method to update resource
+
+        Args:
+            resource_id (str): id of the resource to be updated
+            name (str): name of the resource
+            scopes (list): list of scopes
+        """
+        # prepare payload
+        payload = {
+            'name': name,
+            'scopes': scopes,
+        }
+
+        # prepare endpoint
+        endpoint = self.config['resource_endpoint'] + resource_id
+
+        # update resource
+        response = requests.get(endpoint, json=payload, headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def delete_resource(self, resource_id):
+        """
+        Method to delete resource
+
+        Args:
+            resource_id (str): id of the resource to be deleted
+        """
+
+        # prepare endpoint
+        endpoint = self.config['resource_endpoint'] + resource_id
+
+        # create resource
+        response = requests.delete(endpoint, headers=self.headers)
         response.raise_for_status()
 
         return response.json()
