b72f73fd211a3a8a1cae4e6f6faf3ae5969880e6
Akhil Lawrence <akhilputhiry@gmail.com>
2019-04-01 08:42:30 +0530

    add test cases for openid

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,5 @@
 #! -*- coding: utf-8 -*-
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from .fixtures import keycloak_client
 
@@ -14,18 +14,22 @@ def test_basic_authorization_header(keycloak_client):
 @patch('keycloak.authorization.requests.post')
 def test_retrieve_rpt(mock_post, keycloak_client):
     """ Test case for retrieve_rpt """
+    mock_post.return_value.json = MagicMock()
     payload = {'grant_type': 'urn:ietf:params:oauth:grant-type:uma-ticket', 'audience': 'flask-app'}
     headers = {'Authorization': 'Bearer token123456789'}
     token_endpoint = 'https://keycloak.dev.lti-mosaic.com/auth/realms/akhil-poc/protocol/openid-connect/token'
     keycloak_client.retrieve_rpt('token123456789')
     mock_post.assert_called_once_with(token_endpoint, data=payload, headers=headers)
+    mock_post.return_value.json.assert_called_once()
 
 
 @patch('keycloak.authorization.requests.post')
 def test_validate_rpt(mock_post, keycloak_client):
     """ Test case for validate_rpt """
+    mock_post.return_value.json = MagicMock()
     payload = {'token_type_hint': 'requesting_party_token', 'token': 'token123456789'}
     headers = {'Authorization': keycloak_client.basic_authorization_header}
     introspection_endpoint = 'https://keycloak.dev.lti-mosaic.com/auth/realms/akhil-poc/protocol/openid-connect/token/introspect'
     keycloak_client.validate_rpt('token123456789')
     mock_post.assert_called_once_with(introspection_endpoint, data=payload, headers=headers)
+    mock_post.return_value.json.assert_called_once()
---
+++ tests/test_openid.py
@@ -0,0 +1,37 @@
+#! -*- coding: utf-8 -*-
+import urllib
+from unittest.mock import patch, MagicMock
+
+from .fixtures import keycloak_client
+
+
+@patch('keycloak.openid.uuid.uuid4')
+def test_authentication_url(mock_uuid4, keycloak_client):
+    """ Test case for authentication_url """
+    mock_uuid4.return_value = '862e94e4-e04b-463b-8d08-577161684b76'
+    arguments = urllib.parse.urlencode({
+        'state': '862e94e4-e04b-463b-8d08-577161684b76',
+        'client_id': keycloak_client.config['client_id'],
+        'response_type': 'code',
+        'scope': 'openid email profile user_roles',
+        'redirect_uri': keycloak_client.config['redirect_uri']
+    })
+    authentication_url = keycloak_client.config['authentication_endpoint'] + '?' + arguments
+    assert authentication_url is not None
+    assert authentication_url == keycloak_client.authentication_url
+
+
+@patch('keycloak.openid.requests.post')
+def test_authentication_callback(mock_post, keycloak_client):
+    """ Test case for authentication_callback """
+    mock_post.return_value.json = MagicMock()
+    payload = {
+        'code': 'code123456789',
+        'grant_type': 'authorization_code',
+        'client_id': keycloak_client.config['client_id'],
+        'redirect_uri': keycloak_client.config['redirect_uri'],
+        'client_secret': keycloak_client.config['client_secret'],
+    }
+    keycloak_client.authentication_callback(code='code123456789')
+    mock_post.assert_called_once_with(keycloak_client.config['token_endpoint'], data=payload)
+    mock_post.return_value.json.assert_called_once()
