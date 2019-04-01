cba66a8f2c54ba1cc6d314a048c400f496586b7a
Akhil Lawrence <akhilputhiry@gmail.com>
2019-04-01 08:25:14 +0530

    add test cases for authorization

---
+++ tests/fixtures.py
@@ -0,0 +1,12 @@
+#! -*- coding: utf-8 -*-
+import os
+import pytest
+from keycloak import KeycloakClient
+
+
+@pytest.fixture
+def keycloak_client():
+    """ fixture for keycloak client """
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    config_file = os.path.join(current_dir, 'keycloak.json')
+    return KeycloakClient(config_file=config_file)
---
+++ tests/test_authorization.py
@@ -0,0 +1,31 @@
+#! -*- coding: utf-8 -*-
+from unittest.mock import patch
+
+from .fixtures import keycloak_client
+
+
+def test_basic_authorization_header(keycloak_client):
+    """ Test case for basic_authorization_header """
+    authorization = keycloak_client.basic_authorization_header
+    assert authorization is not None
+    assert authorization == 'Basic Zmxhc2stYXBwOjFmNDlmMDU3LWJiZjktNDM4OS1hOTBmLTNjNTk3MmY1NTY0YQ=='
+
+
+@patch('keycloak.authorization.requests.post')
+def test_retrieve_rpt(mock_post, keycloak_client):
+    """ Test case for retrieve_rpt """
+    payload = {'grant_type': 'urn:ietf:params:oauth:grant-type:uma-ticket', 'audience': 'flask-app'}
+    headers = {'Authorization': 'Bearer token123456789'}
+    token_endpoint = 'https://keycloak.dev.lti-mosaic.com/auth/realms/akhil-poc/protocol/openid-connect/token'
+    keycloak_client.retrieve_rpt('token123456789')
+    mock_post.assert_called_once_with(token_endpoint, data=payload, headers=headers)
+
+
+@patch('keycloak.authorization.requests.post')
+def test_validate_rpt(mock_post, keycloak_client):
+    """ Test case for validate_rpt """
+    payload = {'token_type_hint': 'requesting_party_token', 'token': 'token123456789'}
+    headers = {'Authorization': keycloak_client.basic_authorization_header}
+    introspection_endpoint = 'https://keycloak.dev.lti-mosaic.com/auth/realms/akhil-poc/protocol/openid-connect/token/introspect'
+    keycloak_client.validate_rpt('token123456789')
+    mock_post.assert_called_once_with(introspection_endpoint, data=payload, headers=headers)
